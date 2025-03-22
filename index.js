2022bbd3c683e268587684fea1c8ed79bd00e6b2
SaiOwO <70618173+SaiOwO@users.noreply.github.com>
2025-03-21 20:17:18 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const gameVariables = {};
+
+io.on("connection", (socket) => {
+    console.log("Nuevo cliente conectado:", socket.id);
+
+    setTimeout(() => {
+        socket.emit("sync", gameVariables);
+        console.log("Sync enviada a", socket.id);
+	console.log("DATA:", gameVariables);
+
+    }, 1000);
+    
+    socket.on("command", (data) => {
+        console.log("Comando:", data);
+	
+	const partes = data.split("|");
+
+        const tipo = String(partes[1]);
+        const para = String(partes[2]);
+        if (tipo == "Timer" && para == "All") {
+            const spliteado = String(partes[3]).split("~");
+            
+            const base = String(partes[0]);
+            gameVariables[base + "m_timerEnabled"] = true;
+            gameVariables[base + "m_startTime"] = Number(spliteado[0]);
+            gameVariables[base + "m_seconds"] = Number(spliteado[1]);
+        }
+
+        io.emit("command", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Cliente desconectado:", socket.id);
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor WebSocket corriendo: ${PORT}`);
+});
